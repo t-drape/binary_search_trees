eec34c485a17fd8480f3c120ceaf874004f26f97
t-drape <thebatreview1@gmail.com>
2024-09-12 15:01:17 -0700

    Create the BstNode class for nodes

---
+++ lib/bst_node.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# From TOP Binary Search Tree Project
+
+# A class to model a node in the Binary Search Tree data structure
+class BstNode
+  attr_accessor :left_val, :right_val
+
+  def initialize(val, left_val = nil, right_val = nil)
+    @val = val
+    @left_val = left_val
+    @right_val = right_val
+  end
+
+  def value
+    @val
+  end
+end
